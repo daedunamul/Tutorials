20d2e1d4855322a9004ad7feb7f0d94be942c07d
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-05-31 15:27:55 +0900

    Create tutNode.h

---
+++ datastructure/tutNode.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+struct tutNode
+{
+	int Value ;
+	struct tutNode *Left , *Right ;
+} ;
+struct tutNodePool
+{
+	int MaxCount , Count ;
+	struct tutNode *FirstAddress , *LastAddress ;
+	struct tutNode **NodeStack ;
+} ;
+
+struct tutNodePool* tutNodePoolcreate( int MaxCount ) ;
+void tutNodePooldelete( struct tutNodePool* NodePool ) ;
+
+struct tutNode* tutNodePoolallocate( struct tutNodePool* NodePool ) ;
+void tutNodePooldeallocate( struct tutNodePool* NodePool , struct tutNode *UsedNode ) ;
