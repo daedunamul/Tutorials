c83917b8f4c954cd4fddaafcc6ee01c34ced9f33
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-06-01 00:55:40 +0900

    Update and rename datastructure/tutNode.h to datastructure/tutLinear/tutLinearNode.h

---
+++ datastructure/tutLinear/tutLinearNode.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdlib.h>
+
+struct tutLinearNode
+{
+	int Value ;
+	struct tutLinearNode *Left , *Right ;
+} ;
+struct tutLinearNodePool
+{
+	int MaxCount , Count ;
+	struct tutLinearNode *FirstAddress , *LastAddress ;
+	struct tutLinearNode **LinearNodeStack ;
+} ;
+
+struct tutLinearNodePool* tutLinearNodePool_create( int MaxCount ) ;
+void tutLinearNodePool_delete( struct tutLinearNodePool* LinearNodePool ) ;
+
+struct tutLinearNode* tutLinearNodePool_allocate( struct tutLinearNodePool* LinearNodePool ) ;
+void tutLinearNodePool_deallocate( struct tutLinearNodePool* LinearNodePool , struct tutNode *UsedLinearNode ) ;
