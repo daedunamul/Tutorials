afd6429995e5866da704b766b37232c5667d34cd
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-06-01 01:00:19 +0900

    Update and rename datastructure/tutNode.c to datastructure/tutLinear/tutLinearNode.c

---
+++ datastructure/tutLinear/tutLinearNode.c
@@ -0,0 +1,54 @@
+#include "tutLinearNode.h"
+
+struct tutLinearNodePool* tutLinearNodePool_create( int MaxCount )
+{
+	struct tutLinearNodePool *NewLinearNodePool = ( struct tutLinearNodePool* )malloc( sizeof( struct tutLinearNodePool ) ) ;
+	if( NewLinearNodePool == NULL )
+		return NULL ;
+		
+	NewLinearNodePool->MaxCount = MaxCount ;
+	NewLinearNodePool->FirstAddress = ( struct tutLinearNode* )malloc( MaxCount * sizeof( struct tutLinearNode ) ) ;
+	if( NewLinearNodePool->FirstAddress == NULL )
+	{
+		free( NewLinearNodePool ) ;
+		return NULL ;
+	}
+	NewLinearNodePool->LastAddress = NewLinearNodePool->FirstAddress * ( MaxCount - 1 ) ;
+	NewLinearNodePool->LinearNodeStack = ( struct tutLinearNode** )malloc( MaxCount * sizeof( struct tutLinearNode* ) ) ;
+	if( NewLinearNodePool->LinearNodeStack == NULL )
+	{
+		free( NewLinearNodePool->FirstAddress ) ;
+		free( NewLinearNodePool ) ;
+		return NULL ;
+	}
+	for( NewLinearNodePool->Count = 0 ; NewLinearNodePool->Count < MaxCount ; NewLinearNodePool->Count ++ )
+		NewLinearNodePool->LinearNodeStack[ NewLinearNodePool->Count ] = NewLinearNodePool->FirstAddress + NewLinearNodePool->Count ;
+	
+	return NewLinearNodePool ;
+}
+void tutLinearNodePool_delete( struct tutLinearNodePool* LinearNodePool )
+{
+	if( LinearNodePool == NULL )
+		return ;
+	
+	free( LinearNodePool->LinearNodeStack ) ;
+	free( LinearNodePool->FirstAddress ) ;
+	free( LinearNodePool ) ;
+}
+
+struct tutLinearNode* tutLinearNodePool_allocate( struct tutLinearNodePool* LinearNodePool )
+{
+	if( LinearNodePool == NULL || LinearNodePool->Count <= 0 )
+		return NULL ;
+	
+	LinearNodePool->Count -- ;
+	return LinearNodePool->LinearNodeStack[ LinearNodePool->Count ] ;
+}
+void tutLinearNodePool_deallocate( struct tutLinearNodePool* LinearNodePool , struct tutLinearNode *UsedLinearNode )
+{
+	if( LinearNodePool == NULL || LinearNodePool->Count >= LinearNodePool->MaxCount )
+		return NULL ;
+	
+	LinearNodePool->LinearNodeStack[ LinearNodePool->Count ] = UsedLinearNode ;
+	LinearNodePool->Count ++ ;
+}
