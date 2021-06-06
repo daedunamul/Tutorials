ab6cbf9715f03a5c0a99f81bc88ad9fa2c5a9649
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-06-06 11:59:04 +0900

    Update tutTree.h

--- datastructure/tutTree/tutTree.h
+++ datastructure/tutTree/tutTree.h
@@ -4,4 +4,5 @@
 void tutTree_travel( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) ) ;
 
 void tutTree_push( struct tutTreeNode *ThisNode , struct tutTreeNodePool *NodePool , int Value ) ;
-void tutTree_pop( struct tutTreeNode *ThisNode , void *Data ) ;
+void tutTree_pop( struct tutTreeNode *ThisNode , struct tutTreeNodePool *NodePool ) ;
+void tutTree_flush( struct tutTreeNode *ThisNode , void *Data ) ;
