c78ccc9e351db2b3bb30f4800c5e9b450e74661b
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-06-08 00:53:04 +0900

    Update tutBST.h

--- datastructure/tutTree/tutBST.h
+++ datastructure/tutTree/tutBST.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "tutTreeNode.h"
-#include <stdbool.h>
 
 void tutBST_preorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) ) ;
 void tutBST_inorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) ) ;
 void tutBST_postorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) ) ;
 
-void tutBST_push( struct tutTreeNode *ThisNode , bool Flag , struct tutTreeNodePool *NodePool , int Value ) ;
-void tutBST_pop( struct tutTreeNode *ThisNode , bool Flag , struct tutTreeNodePool *NodePool ) ;
+void tutBST_push( struct tutTreeNode *ThisNode , struct tutTreeNodePool *NodePool , int Value ) ;
+void tutBST_pop( struct tutTreeNode *ThisNode , struct tutTreeNodePool *NodePool ) ;
