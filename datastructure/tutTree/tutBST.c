fe30d80d48b8a7bcd9152e5c24fdb065d4e4279f
대두나물 <49893731+daedunamul@users.noreply.github.com>
2021-06-08 00:28:02 +0900

    Update tutBST.c

--- datastructure/tutTree/tutBST.c
+++ datastructure/tutTree/tutBST.c
@@ -1,7 +1,77 @@
 #include "tutBST.h"
-
-void tutBST_travel( struct tutTreeNode *RootNode , void *Data , enum tutBSTMethod Method , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) )
+void tutBST_preorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) )
 {
+	if( RootNode == NULL )
+		return ;
+	
+	struct tutTreeNode *ThisNode = RootNode ;
+	
+	Visiting : 
+	TravelFx( ThisNode , Data ) ;
+	
+	if( ThisNode->Left != NULL )
+	{
+		ThisNode = ThisNode->Left ;
+		goto Visiting ;
+	}
+	
+	if( ThisNode->Right != NULL )
+	{
+		ThisNode = ThisNode->Right ;
+		goto Visiting ;
+	}
+	
+	while( ThisNode->Super != NULL )
+	{
+		if( ThisNode->Super->Right != NULL && ThisNode->Super->Right != ThisNode )
+		{
+			ThisNode = ThisNode->Super->Right ;
+			goto Visiting ;
+		}
+		
+		ThisNode = ThisNode->Super ;
+	}
+	
+	// no nodes to visit
+}
+void tutBST_inorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) )
+{
+
+}
+void tutBST_postorder( struct tutTreeNode *RootNode , void *Data , void ( *TravelFx )( struct tutTreeNode *ThisNode , void *Data ) )
+{
+	if( RootNode == NULL )
+		return ;
+	
+	struct tutTreeNode *ThisNode = RootNode ;
+	
+	Moving : 
+	if( ThisNode->Left != NULL )
+	{
+		ThisNode = ThisNode->Left ;
+		goto Moving ;
+	}
+	
+	if( ThisNode->Right != NULL )
+	{
+		ThisNode = ThisNode->Right ;
+		goto Moving ;
+	}
+	
+	while( ThisNode->Super != NULL )
+	{
+		TravelFx( ThisNode , Data ) ;
+		
+		if( ThisNode->Super->Right != NULL && ThisNode->Super->Right != ThisNode )
+		{
+			ThisNode = ThisNode->Super->Right ;
+			goto Moving ;
+		}
+		
+		ThisNode = ThisNode->Super ;
+	}
+	
+	// no nodes to visit
 }
 
 void tutBST_push( struct tutTreeNode *ThisNode , bool Flag , struct tutTreeNodePool *NodePool , int Value )
